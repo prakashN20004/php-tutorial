646e26a07510f4abfeaf99389aac5d84a980e2e5
Prakash N <prakashnarayanasamy258@gmail.com>
2026-07-07 17:24:05 +0530

    first commit

---
+++ operators.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <?php
+    $x = 10;
+    $y = 15;
+    $z = null;
+
+    $z = $x + $y;
+    echo "Sum is: " . $z . "<br>";
+    $z = $x - $y;
+    echo "Sub is: " . $z . "<br>";
+    $z = $x * $y;
+    echo "Mul is: " . $z . "<br>";
+    $z = $x / $y;
+    echo "Div is: " . $z . "<br>";
+    $z = $x ** $y;
+    echo "Power is: " . $z . "<br>";
+    $z = $x % $y;
+    echo "Remainder is: " . $z . "<br>";
+    
+    ?> -->
+
+    <!-- <?php
+    //Increment/Decrement Operators
+
+    $counters = 0;
+
+    $counters+=3;
+
+    echo $counters;
+
+    ?> -->
+
+    <?php
+    $total = 1 + 2 - 3 * 4 / (5 ** 6) + 6;
+
+    echo $total;
+
+    ?>
+</body>
+</html>
